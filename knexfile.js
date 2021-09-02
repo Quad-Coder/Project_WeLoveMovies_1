a8e293efbe6f6368c2bb89f9576e24d23985ae46
Quadrick <quadtreeder@gmail.com>
2021-09-02 11:40:50 -0700

    updated code

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,8 @@ const path = require("path");
 
 require("dotenv").config();
 
-const {
-   DATABASE_URL = "postgres://xujoydei:WlqEBhOAnzftYwAr7AYfE-uP73hz4fXw@kashin.db.elephantsql.com/xujoydei",
-} = process.env;
+const { DATABASE_URL = "postgresql://postgres@localhost/postgres" } =
+  process.env;
 
 module.exports = {
   development: {
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,43 +1,42 @@
 const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
-const knex = require("../db/connection");
 
 const addCriticDetails = mapProperties({
-    preferred_name: "critic.preferred_name",
-    surname: "critic.surname",
-    organization_name: "critic.organization_name",
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
 });
 
 function read(reviewId) {
-    return knex("reviews").select("*").where({ review_id: reviewId }).first();
+  return knex("reviews").select("*").where({ review_id: reviewId }).first();
 }
 
 function update(updatedReview) {
-    return knex("reviews")
+  return knex("reviews")
     .select("*")
     .where({ review_id: updatedReview.review_id })
     .update(updatedReview);
 }
 
 function getReviewsWithCritic(reviewId) {
-    return knex("reviews as r")
+  return knex("reviews as r")
     .join("critics as c", "r.critic_id", "c.critic_id")
     .select("*")
     .where({ review_id: reviewId })
     .first()
     .then((result) => {
-        const updatedReview = addCriticDetails(result);
-        return updatedReview;
-    })
+      const updatedReview = addCriticDetails(result);
+      return updatedReview;
+    });
 }
 
 function destroy(reviewId) {
-    return knex("reviews").where({ review_id: reviewId }).del();
+  return knex("reviews").where({ review_id: reviewId }).del();
 }
 
 module.exports = {
-    update,
-    getReviewsWithCritic,
-    read,
-    destroy,
+  update,
+  getReviewsWithCritic,
+  read,
+  destroy,
 };
